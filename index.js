632d8b65fbd28628d77efe677d24a256557762f9
Saurav Subedi <subedisaurav331@gmail.com>
2026-06-25 22:23:04 +0900

    Added array for kanji and meaning

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const Kanji = ["日", "月", "火", "水", "木", "金", "土", "山", "川", "人"];
+const Kanji = [
+  { kanji: "日", meaning: "Day" },
+  { kanji: "月", meaning: "Month" },
+  { kanji: "火", meaning: "fire" },
+  { kanji: "水", meaning: "water" },
+  { kanji: "木", meaning: "tree" },
+  { kanji: "金", meaning: "gold" },
+  { kanji: "土", meaning: "land" },
+  { kanji: "山", meaning: "mountain" },
+  { kanji: "川", meaning: "river" },
+  { kanji: "人", meaning: "person" },
+];
 let index = 0;
 const Card = document.querySelector(".card");
 const Next = document.querySelector(".next");
@@ -9,7 +20,7 @@ Next.addEventListener("click", function () {
   if (index > 9) {
     index = 0;
   }
-  Card.textContent = Kanji[index];
+  Card.textContent = Kanji[index].kanji;
 });
 
 Prev.addEventListener("click", function () {
@@ -17,5 +28,5 @@ Prev.addEventListener("click", function () {
   if (index < 0) {
     index = 9;
   }
-  Card.textContent = Kanji[index];
+  Card.textContent = Kanji[index].kanji;
 });
